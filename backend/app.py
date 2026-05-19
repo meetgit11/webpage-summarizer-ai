fce0ec4468656837637b6d43d27cc44bfc6f83ca
Meet Patil <meetpatil1104@gmail.com>
2026-05-19 20:35:59 +0530

    added summarize route

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route("/")
 def home():
     return "Backend is working!"
+
+@app.route("/summarize", methods=["POST"])
+def summarize():
+
+    data = request.get_json()
+
+    url = data.get("url")
+
+    return jsonify({
+        "message": "Summarize route working",
+        "url_received": url
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
