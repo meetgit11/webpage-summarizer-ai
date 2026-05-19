97bb7b2efff9ae4d7c11a4e117b5e81a7c1aa73a
Meet Patil <meetpatil1104@gmail.com>
2026-05-19 20:25:54 +0530

    clean redeploy

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,10 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask
 
 app = Flask(__name__)
 
-CORS(app)
-
 @app.route("/")
 def home():
-    return jsonify({
-        "message": "Backend Working Successfully"
-    })
-
-@app.route("/summarize", methods=["POST"])
-def summarize():
-    return jsonify({
-        "summary": "Test summary working"
-    })
+    return "Backend is working!"
 
 if __name__ == "__main__":
     app.run(debug=True)
