dfc93adfa38251f4984f84966c1ad458965ed705
Meet Patil <meetpatil1104@gmail.com>
2026-05-19 16:36:42 +0530

    Fixed Backend

--- backend/app.py
+++ backend/app.py
@@ -1,52 +1,21 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from flask_cors import CORS
-
-from scraper import scrape_website
-from summarizer import generate_summary
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "*"}})
-
+CORS(app)
 
 @app.route("/")
 def home():
     return jsonify({
-        "message": "Backend is running"
+        "message": "Backend Working"
     })
-
 
 @app.route("/summarize", methods=["POST"])
 def summarize():
-
-    try:
-        data = request.get_json()
-
-        if not data or "url" not in data:
-            return jsonify({
-                "error": "URL is required"
-            }), 400
-
-        url = data["url"]
-
-        scraped_text = scrape_website(url)
-
-        if not scraped_text:
-            return jsonify({
-                "error": "Could not scrape webpage"
-            }), 400
-
-        summary = generate_summary(scraped_text)
-
-        return jsonify({
-            "summary": summary
-        })
-
-    except Exception as e:
-        return jsonify({
-            "error": str(e)
-        }), 500
-
+    return jsonify({
+        "summary": "Test successful"
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
